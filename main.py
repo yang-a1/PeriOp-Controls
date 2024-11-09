bb3f0f1e23918e9973f61804e9bfb287b3a7adfa
yang-a1 <amyhyang24@gmail.com>
2024-11-09 07:38:53 -0500

    Temperature Reading v1.0.0

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import Adafruit_DHT
+import time
 
-# Sensor should be set to Adafruit_DHT.DHT11,
-# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+# Sensor should be set to Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.DHT22
 
-# Example using a Beaglebone Black with DHT sensor
-# connected to pin P8_11.
+# GPIO pin connected to the sensor data line
 pin = '17'
 
-# Example using a Raspberry Pi with DHT sensor
-# connected to GPIO23.
-#pin = 23
+try:
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print('Failed to get reading. Try again!')
+        
+        time.sleep(2)
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
+except KeyboardInterrupt:
+    print("\nProgram stopped by user")
