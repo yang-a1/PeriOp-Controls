083ee63725f03703e8e2712c7167a6246a8e0be6
Madelynn Camarillo <madelynnsmacbook@0587453352.wireless.umich.net>
2025-02-23 13:44:36 -0500

    Just added Relay.py

---
+++ Relay.py
@@ -0,0 +1,5 @@
+# label pin of usage to indicate yellow (info) wire
+# initialize connnection
+# turn on (HIGH) every two seconds
+# turn off (LOW) every two seconds
+# while loop (with two seconds condition)
